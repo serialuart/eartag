ad8eadd6a98251a2e6386372486b577c22cf19fa
knuxify <knuxify@gmail.com>
2026-08-21 15:02:28 +0200

    tests: remove unused workaround

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,6 @@ import pytest
 def pytest_configure(config):
     config.addinivalue_line("markers", "networked_tests: enable MusicBrainz and AcoustID test")
 
-    import sys
-
-    sys._called_from_test = True
-
 
 def pytest_addoption(parser):
     parser.addoption(
@@ -20,18 +16,3 @@ def pytest_addoption(parser):
 def pytest_runtest_setup(item):
     if "networked_tests" in item.keywords and not item.config.getoption("--networked_tests"):
         pytest.skip("need --networked_tests option to run this test")
-
-
-def pytest_unconfigure(config):
-    import sys  # This was missing from the manual
-
-    del sys._called_from_test
-
-
-@pytest.fixture(autouse=True)
-def patch_config(monkeypatch):
-    import src.config
-
-    from .common import config
-
-    monkeypatch.setattr(src.config, "config", config)
